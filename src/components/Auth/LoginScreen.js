aa875472783bb3db8dfde2cfc79c57164c050dcf
chatnq <chatnq@gmail.com>
2018-08-06 23:55:31 +0700

    Signed-off-by: chatnq <chatnq@gmail.com>

--- src/components/Auth/LoginScreen.js
+++ src/components/Auth/LoginScreen.js
@@ -9,17 +9,141 @@ import {
     StyleSheet, 
 } from 'react-native';
 import { NavigationActions } from 'react-navigation'
-import Icon from 'react-native-vector-icons/Ionicons'
+import Icon from 'react-native-vector-icons/Ionicons';
+import { Divider, TouchableOpacity } from '@shoutem/ui'
+import dismissKeyboard from 'react-native/Libraries/Utilities/dismissKeyboard';
 import colors from '../../util/colors'
 import Strings from '../../util/string'
 import logoGo from '../../images/logo.png'
+
+const styles =  {
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    flexDirection: 'column',
+    alignItems: 'center',
+    paddingTop: 60,
+  },
+  inputType: {
+    padding: 5,
+    alignItems: 'center',
+    width: 270,
+    borderRadius: 4,
+    borderColor: colors.primaryColor,
+    flexDirection: 'row',
+  },
+  iconStyle: {
+    marginRight: 10,
+  },
+  loginStyle: {
+    width: 270,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logoStyle: {
+    marginTop: 40,
+    marginBottom: 150,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 200,
+    height: 100,
+  },
+  enableButton: {
+    backgroundColor: colors.primaryColor,
+    padding: 10,
+    opacity: 1,
+    paddingLeft: 20,
+    paddingRight: 20,
+    width: 270,
+    borderRadius: 20,
+    shadowOffset: { width: 2, height: 2 },
+    shadowColor: colors.primaryColor,
+    shadowOpacity: 0.7,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  disabledButton: {
+    backgroundColor: colors.primaryColor,
+    opacity: 1,
+    padding: 10,
+    paddingLeft: 20,
+    paddingRight: 20,
+    width: 270,
+    borderRadius: 20,
+    shadowOffset: { width: 2, height: 2 },
+    shadowColor: colors.primaryColor,
+    shadowOpacity: 0.7,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+};
 
 export default class LoginScreen extends Component {
     static navigationOptions = {
         title: 'Login',
       }
+      constructor(props) {
+        super(props);
+        this.state = { username: 'Nhập tên đăng nhập.' };
+        this.state = { password: 'Nhập mật khẩu.' };
+      }
      render() {
+      const {
+        container,
+        inputType,
+        iconStyle,
+        loginStyle,
+        logoStyle,
+      } = styles
+
         return (
+          <TouchableWithoutFeedback style={{ flex: 1 }} onPress={dismissKeyboard}>
+          <KeyboardAvoidingView style={container} behavior="padding" enabled>
+            <View style={logoStyle}>
+              <Image
+                source={logoGo}
+                resizeMode="stretch"
+                style={{
+                  width: 154.63,
+                  height: 120,
+                }}
+              />
+            </View>
+            <View style={loginStyle}>
+              <View style={inputType}>
+                <TextInput
+                  style={{ fontSize: 14, height: 40, borderColor: 'gray', borderWidth: 1 }}
+                  placeholder='Nhập tên đăng nhập'
+                  onChangeText={username => this.setState({ username })}                
+                />
+             </View>          
+             <View style={inputType}>
+                <TextInput
+                  style={{ fontSize: 14 }}
+                  placeholder='Nhập mật khẩu'
+                  onChangeText={password => this.setState({ password })}                
+                />
+             </View>    
+              <TouchableOpacity
+                style={styles.button}
+                onPress={this.onPress}
+                >
+                <Text style={{ color: '#f2f', fontWeight: 'bold' }}>Đăng nhập </Text>
+              </TouchableOpacity>
+              <Text
+                style={{
+                  paddingTop: 10,
+                  paddingBottom: 20,
+                  textAlign: 'center',
+                  color: colors.textColorGray,
+                }}
+              >
+                'Quên mật khẩu'
+              </Text>
+            </View>
+          </KeyboardAvoidingView>
+        </TouchableWithoutFeedback>
+        /*
         <View>  
             <View>
                 <Text> Login Page </Text>
@@ -28,69 +152,10 @@ export default class LoginScreen extends Component {
                 <Text> Loading... </Text>
             </View>
         </View>
+        */
         )
     }
 }
 
-const styles =  StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      flexDirection: 'column',
-      alignItems: 'center',
-      paddingTop: 60,
-    },
-    inputType: {
-      padding: 5,
-      alignItems: 'center',
-      width: 270,
-      borderRadius: 4,
-      borderColor: colors.primaryColor,
-      flexDirection: 'row',
-    },
-    iconStyle: {
-      marginRight: 10,
-    },
-    loginStyle: {
-      width: 270,
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    logoStyle: {
-      marginTop: 40,
-      marginBottom: 150,
-      alignItems: 'center',
-      justifyContent: 'center',
-      width: 200,
-      height: 100,
-    },
-    enableButton: {
-      backgroundColor: colors.primaryColor,
-      padding: 10,
-      opacity: 1,
-      paddingLeft: 20,
-      paddingRight: 20,
-      width: 270,
-      borderRadius: 20,
-      shadowOffset: { width: 2, height: 2 },
-      shadowColor: colors.primaryColor,
-      shadowOpacity: 0.7,
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    disabledButton: {
-      backgroundColor: colors.primaryColor,
-      opacity: 1,
-      padding: 10,
-      paddingLeft: 20,
-      paddingRight: 20,
-      width: 270,
-      borderRadius: 20,
-      shadowOffset: { width: 2, height: 2 },
-      shadowColor: colors.primaryColor,
-      shadowOpacity: 0.7,
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
+
   
--- src/components/Auth/Walkthroughscreen.js
+++ src/components/Auth/Walkthroughscreen.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-import { View, Button, Text, StyleSheet } from 'react-native';
+import { View, Button, Text, StyleSheet, Image, TouchableHighlight, SafeAreaView } from 'react-native';
+import ImageSlider from 'react-native-image-slider';
+//import img1 from '../../images/1.jpg';
+//import img2 from '../../images/2.jpg';
 
 
 export default class Walkthrough extends Component {
@@ -12,27 +15,67 @@ export default class Walkthrough extends Component {
         console.log('Click Register...');
     };
     render() {
+        const images = [
+           
+            'https://placeimg.com/640/640/animals',
+            'https://placeimg.com/640/640/beer',
+          ];
         return (
-        <View style={styles.container}>  
-            <View>
+        //<View style={styles.container}>
+        <SafeAreaView style={styles.container}>  
+            <View style={styles.slide}>
                 <Text> Loading... </Text>
+                <ImageSlider
+                    loopBothSides
+                    autoPlayWithInterval={3000}
+                    images={images}
+                    customSlide={({ index, item, style, width }) => (
+                        // It's important to put style here because it's got offset inside
+                        <View key={index} style={[style, styles.customSlide]}>
+                        <Image source={{ uri: item }} style={styles.customImage} />
+                        </View>
+                    )}
+                    customButtons={(position, move) => (
+                        <View style={styles.buttons}>
+                        {images.map((image, index) => {
+                            return (
+                            <TouchableHighlight
+                                key={index}
+                                underlayColor="#ccc"
+                                onPress={() => move(index)}
+                                style={styles.button}
+                            >
+                                <Text style={position === index && styles.buttonSelected}>
+                                {index + 1}
+                                </Text>
+                            </TouchableHighlight>
+                            );
+                        })}
+                        </View>
+                    )}
+                    />
             </View>
-            <View>
-                <Text> Walkthrough Page </Text>
+            <View style={styles.links}>
+                <Text> Tìm hiểu về medihome </Text>
             </View>            
-            <View style={styles.button}>
+            <View style={styles.links}>
+                    <View style={{flex:1, paddingRight: 5,  }}>
                     <Button 
                         onPress={this.onClickLogin.bind(this)}                      
                         title="Đăng nhập"                       
                     />
-            </View> 
-            <View style={styles.button}>
+                    </View>
+                    <View style={{flex:1, paddingLeft: 5, }}>
                     <Button 
                         onPress={this.onClickRegister.bind(this)}                      
                         title="Đăng Ký"                       
                     />
+                    </View>
+            </View> 
+            <View style={styles.guide}>
+                   <Text>About us</Text> 
             </View>    
-        </View>
+        </SafeAreaView>
         )
     }
 }
@@ -40,16 +83,30 @@ const styles = StyleSheet.create({
     container: {
       flex: 1,
       justifyContent: 'center',
-      alignContent: 'center',
+      //alignContent: 'center',
       alignItems: 'center',
       paddingHorizontal: 10,      
     },
-    button: {
-      alignItems: 'center',
-      backgroundColor: '#DDDDDD',      
-      height: 40, 
-      width: 200,
-      alignContent:'center'
-    },
+    slide: {
+        flex: 7,
+        alignItems: 'center',
+        backgroundColor: '#DDDDDD', 
+        alignContent:'center'
+      },
+    links: {
+        flex: 1,
+        alignItems: 'center',
+        backgroundColor: '#DDDDDD', 
+        alignContent:'center',
+        flexDirection: 'row'
+      },
+   
+    guide: {
+        flex: 1,
+        alignItems: 'center',
+        backgroundColor: '#DDDDDD',       
+        alignContent:'center',
+        paddingBottom: 5,
+      },
     
 })
